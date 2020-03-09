61d69a1d244e79feea9232467f0eab305c05e0a6
jchuru <jameschuru123@gmail.com>
2020-03-09 12:33:32 -0400

    Final commit

--- src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Lab.java
@@ -4,18 +4,21 @@ package rocks.zipcode.io.quiz3.collections;
  * @author leon on 10/12/2018.
  */
 public class Lab {
+    String name;
     public Lab() {
-        this(null);
+        this.name = null;
     }
 
     public Lab(String labName) {
+        this.name = labName;
     }
 
     public String getName() {
-        return null;
+        return this.name;
     }
 
     public void setStatus(LabStatus labStatus) {
+
     }
 
     public LabStatus getStatus() {
